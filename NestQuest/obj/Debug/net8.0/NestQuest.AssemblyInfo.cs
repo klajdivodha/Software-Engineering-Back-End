7d44369f61cfe54a7de1a0b3d57caa1a30f62272
klajdivodha <kvodha21@epoka.edu.al>
2024-03-26 19:02:58 +0100

    config

--- NestQuest/obj/Debug/net8.0/NestQuest.AssemblyInfo.cs
+++ NestQuest/obj/Debug/net8.0/NestQuest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NestQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90e90790daaa16966104ecfbd8f2dd5576eea06")]
 [assembly: System.Reflection.AssemblyProductAttribute("NestQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NestQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
